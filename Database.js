d781f7fd6d4ff696c60d9c8dcc629655407d26b3
abj134 <amazonbkr@gmail.com>
2025-11-04 23:22:36 +0500

    try to change database file

--- Database.js
+++ Database.js
@@ -1,12 +1,21 @@
-const {Sequelize} = require('sequelize');
-require(`dotenv`).config();
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USER,process.env.DB_PASSWORD,{
-    host:process.env.DB_HOST,
-    dialect: 'postgres'
-})
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, 
+      },
+    },
+    logging: false,
+  }
+);
 
-sequelize.authenticate()
-.then(()=>console.log('Database connected'))
-.catch(error => console.error('Error connecting database',error));
 module.exports = sequelize;
